11dc6ab3f7efdd8847484f8191266acff906c818
sampr <sampritchard428@gmail.com>
2025-12-02 10:47:48 -0500

    Initial commit

---
+++ src/client/java/com/evonimals/EvonimalsClient.java
@@ -0,0 +1,10 @@
+package com.evonimals;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvonimalsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
